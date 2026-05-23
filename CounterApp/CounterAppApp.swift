cb724fd10cc3689ca62620e061bd97bd387a1182
RitoVolley <200110258+RitoVolley@users.noreply.github.com>
2026-05-23 09:45:25 +0900

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by RitoVolley on 2026/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
